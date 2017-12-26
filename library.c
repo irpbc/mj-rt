b8d655b1ce49ea07540780b54a659003d08237ca
ivan <ivan295@gmail.com>
2017-12-26 12:08:39 +0100

    Added binding for printf with varargs.

--- library.c
+++ library.c
@@ -1,16 +1,8 @@
 #include "library.h"
 
 #include <stdio.h>
+#include <stdarg.h>
 
-
-#define EXPORT __declspec(dllexport)
-
-/**
- * Called as DllImport from .net to trigger library load.
- */
-EXPORT void mj_init() {
-
-}
 
 EXPORT void mj_hello() {
     printf("Hello, World!\n");
@@ -24,6 +16,9 @@ EXPORT int mj_puts(const char* str) {
     return puts(str);
 }
 
-EXPORT int mj_printf_int(const char* str, int arg) {
-    return printf(str, arg);
+EXPORT int mj_printf(const char* str, ...) {
+
+    va_list args;
+    va_start(args, str);
+    return vprintf(str, args);
 }
--- library.h
+++ library.h
@@ -1,12 +1,15 @@
 #ifndef MJ_RT_LIBRARY_H
 #define MJ_RT_LIBRARY_H
 
-#define EXPORT __declspec(dllexport)
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT
+#endif
 
-EXPORT void mj_init();
 EXPORT void mj_hello(void);
 EXPORT int mj_putchar(int c);
 EXPORT int mj_puts(const char* str);
-EXPORT int mj_printf_int(const char* str, int arg);
+EXPORT int mj_printf(const char* str, ...);
 
 #endif
