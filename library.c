0e17f657d3f10461207b4104978f74411a9226b2
ivan <ivan295@gmail.com>
2017-12-26 19:53:23 +0100

    Added specialized scanf bindings.

--- library.c
+++ library.c
@@ -2,23 +2,48 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <inttypes.h>
 
 
 EXPORT void mj_hello() {
     printf("Hello, World!\n");
 }
 
-EXPORT int mj_putchar(int c) {
+EXPORT int32_t mj_putchar(int32_t c) {
     return putchar(c);
 }
 
-EXPORT int mj_puts(const char* str) {
+EXPORT int32_t mj_puts(const char* str) {
     return puts(str);
 }
 
-EXPORT int mj_printf(const char* str, ...) {
+EXPORT int32_t mj_printf(const char* str, ...) {
 
     va_list args;
     va_start(args, str);
     return vprintf(str, args);
+}
+
+EXPORT int32_t mj_scan_int() {
+    int32_t x = 0;
+    scanf("%"PRId32, &x);
+    return x;
+}
+
+EXPORT int64_t mj_scan_long() {
+    int64_t x = 0L;
+    scanf("%"PRId64 , &x);
+    return x;
+}
+
+EXPORT float mj_scan_float() {
+    float x = 0.0f;
+    scanf("%f", &x);
+    return x;
+}
+
+EXPORT double mj_scan_double() {
+    double x = 0.0;
+    scanf("%lf", &x);
+    return x;
 }
--- library.h
+++ library.h
@@ -1,6 +1,8 @@
 #ifndef MJ_RT_LIBRARY_H
 #define MJ_RT_LIBRARY_H
 
+#include <stdint.h>
+
 #ifdef _WIN32
     #define EXPORT __declspec(dllexport)
 #else
@@ -8,8 +10,13 @@
 #endif
 
 EXPORT void mj_hello(void);
-EXPORT int mj_putchar(int c);
-EXPORT int mj_puts(const char* str);
-EXPORT int mj_printf(const char* str, ...);
+EXPORT int32_t mj_putchar(int32_t c);
+EXPORT int32_t mj_puts(const char* str);
+EXPORT int32_t mj_printf(const char* str, ...);
+
+EXPORT int32_t mj_scan_int();
+EXPORT int64_t mj_scan_long();
+EXPORT float mj_scan_float();
+EXPORT double mj_scan_double();
 
 #endif
